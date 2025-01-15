ea1c59774b40165fd04c23d171b3a996954bafa2
Alex Warda <alexsio03@gmail.com>
2025-01-15 15:33:26 -0800

    codeium, autoclose, some keybinds

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -32,6 +32,9 @@ return {
       lspconfig.pyright.setup({
         capabilities = capabilities,
       })
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+      })
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
